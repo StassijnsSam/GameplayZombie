cfa207591a22f74a7d95802944f9896d8b2baa09
PH03N1X101 <stassijns.sam@gmail.com>
2023-01-01 14:39:36 +0100

    Added existing framework files for Behaviors and Steering

---
+++ ZombieGame/project/Behaviors.h
@@ -0,0 +1,27 @@
+/*=============================================================================*/
+// Copyright 2020-2021 Elite Engine
+/*=============================================================================*/
+// Behaviors.h: Implementation of certain reusable behaviors for the BT version of the Agario Game
+/*=============================================================================*/
+#ifndef ELITE_APPLICATION_BEHAVIOR_TREE_BEHAVIORS
+#define ELITE_APPLICATION_BEHAVIOR_TREE_BEHAVIORS
+//-----------------------------------------------------------------
+// Includes & Forward Declarations
+//-----------------------------------------------------------------
+#include "EliteMath/EMath.h"
+#include "EBehaviorTree.h"
+#include "SteeringBehaviors.h"
+
+//-----------------------------------------------------------------
+// Behaviors
+//-----------------------------------------------------------------
+namespace BT_Actions
+{
+	
+}
+
+namespace BT_Conditions
+{
+	
+}
+#endif
---
+++ ZombieGame/project/SteeringBehaviors.cpp
@@ -0,0 +1,6 @@
+//Precompiled Header [ALWAYS ON TOP IN CPP]
+#include "stdafx.h"
+
+//Includes
+#include "SteeringBehaviors.h"
+#include "EliteMath\EMatrix2x3.h"
